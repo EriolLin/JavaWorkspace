154c62e7966862635b4559b8ff927adcafcc14f3
EriolLin <tyatyamarunoyume@outlook.com>
2019-03-24 23:13:47 +0800

    modify by 3-24

--- expert-project-ssm-web/src/main/java/com/backoffice/controller/ExpertController.java
+++ expert-project-ssm-web/src/main/java/com/backoffice/controller/ExpertController.java
@@ -5,20 +5,39 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.backoffice.model.Expert;
 import com.backoffice.service.IExpertService;
 
 @Controller
-@RequestMapping("/expert")
+@RequestMapping("expert")
 public class ExpertController {
 	
 	@Autowired
 	IExpertService iExpertService;
 
-	@RequestMapping("/findbyid")
-	public String findbyid(Model model) {
+//	@RequestMapping("findbyid")
+//	public String findbyid(Model model) {
+//		// 查数据
+//		Expert expert = iExpertService.findByid(1);
+//		System.out.println("expert:" + expert);
+////		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+////		String str = sdf.format((Date)expert.getBirth());
+////		System.out.println("birth:"+str);
+//		// 存数据
+//		model.addAttribute("expert", expert);
+//		return "index";
+//	}
+	
+	@RequestMapping("findbyid")
+//	@RequestMapping(value="findbyid", method = RequestMethod.POST)
+//	@GetMapping(value= {"findbyid"})
+	@ResponseBody
+	public String findbyid() {
 		// 查数据
 		Expert expert = iExpertService.findByid(1);
 		System.out.println("expert:" + expert);
@@ -26,11 +45,11 @@ public class ExpertController {
 //		String str = sdf.format((Date)expert.getBirth());
 //		System.out.println("birth:"+str);
 		// 存数据
-		model.addAttribute("expert", expert);
-		return "index";
+		String jsonString = "{\"msg\":\"hello world\"}";
+		return jsonString;
 	}
 	
-	@RequestMapping("/list")
+	@RequestMapping("list")
 	public String list() {
 		List<Expert> experts = iExpertService.findAllExpert();
 		for (Expert expert : experts) {
--- expert-project-ssm-web/src/main/java/com/backoffice/controller/SubjectController.java
+++ expert-project-ssm-web/src/main/java/com/backoffice/controller/SubjectController.java
@@ -1,14 +1,28 @@
 package com.backoffice.controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.backoffice.model.Subject;
 import com.backoffice.service.ISubjectService;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;  
+import com.fasterxml.jackson.databind.JsonNode;  
+import com.fasterxml.jackson.databind.ObjectMapper; 
+
 
 @Controller
 @RequestMapping("subject")
@@ -16,26 +30,71 @@ public class SubjectController {
 	@Autowired
 	ISubjectService iSubjectService;
 
+//	@RequestMapping("findbyid")
+//	public String findbyid(Model model) {
+//		// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+//		Subject Subject = iSubjectService.findByid(1);
+//		System.out.println("Subject:" + Subject);
+////		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+////		String str = sdf.format((Date)Subject.getBirth());
+////		System.out.println("birth:"+str);
+//		// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+//		model.addAttribute("Subject", Subject);
+//		return "index";
+//	}
+	
 	@RequestMapping("findbyid")
-	public String findbyid(Model model) {
-		// ������
-		Subject Subject = iSubjectService.findByid(1);
-		System.out.println("Subject:" + Subject);
-//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-//		String str = sdf.format((Date)Subject.getBirth());
-//		System.out.println("birth:"+str);
-		// ������
-		model.addAttribute("Subject", Subject);
-		return "index";
+	@ResponseBody
+	public String findbyid() {
+		
+
+		Subject subject = iSubjectService.findByid(1);
+		System.out.println("Subject:" + subject);
+		String sjson = "{\"id\":\""+ subject.getId()+"\","+              
+				"\"remarks\":\""+subject.getRemarks()+"\","+ 
+				"\"subjectName\":\""+subject.getSubjectName()+
+				"\"}";
+		System.out.println("sjson"+sjson);
+
+		return sjson;
 	}
 	
+	
+	
+//	@RequestMapping("list")
+//	public String list() {
+//		List<Subject> Subjects = iSubjectService.findAllSubject();
+//		for (Subject Subject : Subjects) {
+//			System.out.println(Subject);
+//		}
+//		return null;
+//	}
+	
 	@RequestMapping("list")
-	public String list() {
-		List<Subject> Subjects = iSubjectService.findAllSubject();
-		for (Subject Subject : Subjects) {
-			System.out.println(Subject);
+	@ResponseBody
+	public List list(@RequestBody Subject subStr) {
+		System.out.println(subStr);
+		
+//		JsonParser parse =new JsonParser(); 
+//		
+//		JsonObject params=(JsonObject) parse.parse(subStr);
+//		System.out.println("jsonid="+params.get("id").getAsString());
+//		
+//		Subject sys = JSONObject.parseObject(subStr,Subject.class);
+//		System.out.println("json="+sys);
+		List<Subject> subjects = iSubjectService.findAllSubject();
+		
+		for (Subject subject : subjects) {
+			System.out.println(subject);
+			String sjson = "{\"id\":\""+ subject.getId()+"\","+              
+					"\"remarks\":\""+subject.getRemarks()+"\","+ 
+					"\"subjectName\":\""+subject.getSubjectName()+
+					"\"}";
+			
 		}
-		return null;
+
+		
+		return subjects;
 	}
 	
 	@RequestMapping("save")
